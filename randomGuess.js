ac10efec240d0d8a1c78495761480436519430c9
Kaisma Penn-Titley <kptitley@gmail.com>
2017-07-29 22:42:24 -0400

    initial HTML, CSS & JS files for random guessing game

---
+++ randomGuess.js
@@ -0,0 +1,6 @@
+var userGuess = document.getElementById("user-guess"), 
+    checkUserGuessButton = document.getElementById("check-user-guess-button"),
+    clearGameButton = document.getElementById("clear-game-button"),
+    displayDiv = document.getElementById("display-div"); 
+
+    alert("JS works on this page");
